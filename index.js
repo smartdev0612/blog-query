f0b7b89bd06d2eb767d0644bd32909519d8a5487
Daniel Morales <danieldev0612@gmail.com>
2022-10-25 18:44:27 -0700

    fetch events when server restarts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const cors = require('cors')
 
 const app = express()
@@ -8,14 +9,27 @@ app.use(cors())
 
 const posts = {}
 
-posts === {
-    'j123j42': {
-        id: 'j123j42',
-        title: 'post title',
-        comments: [
-            { id: 'klj3kl', content: 'comment!' }
-        ]
-    },
+const handleEvent = (type, data) => {
+    if (type === 'PostCreated') {
+        const { id, title } = data
+        posts[id] = { id, title, comments: [] }
+    }
+
+    if (type === 'CommentCreated') {
+        const { id, content, postId, status } = data
+        const post = posts[postId]
+        post.comments.push({ id, content, status })
+    }
+
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data
+        const post = posts[postId]
+        const comment = post.comments.find(comment => {
+            return comment.id === id
+        })
+        comment.status = status
+        comment.content = content
+    }
 }
 
 app.get('/posts', (req, res) => {
@@ -25,22 +39,22 @@ app.get('/posts', (req, res) => {
 app.post('/events', (req, res) => {
     const { type, data } = req.body;
 
-    if (type === 'PostCreated') {
-        const { id, title } = data
-        posts[id] = { id, title, comments: [] }
-    }
-
-    if (type === 'CommentCreated') {
-        const { id, content, postId } = data
-        const post = posts[postId]
-        post.comments.push({ id, content })
-    }
-
-    console.log(posts)
+    handleEvent(type, data)
 
     res.send({})
 })
 
-app.listen(4002, () => {
-    console.log('Listening on 4002')
-})
+app.listen(4002, async () => {
+    console.log("Query service listening on 4002");
+    try {
+        const res = await axios.get("http://localhost:4005/events");
+        
+        for (let event of res.data) {
+            console.log("Processing event:", event.type);
+        
+            handleEvent(event.type, event.data);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+});
